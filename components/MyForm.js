5afa0c8af7251d9a35384e12caa77b2c89095758
Eloka <12972250+elokaagu@users.noreply.github.com>
2022-01-12 23:06:19 +0000

    changes

--- components/MyForm.js
+++ components/MyForm.js
@@ -4,7 +4,22 @@ export default function MyForm(props) {
   return (
     <div className="relative w-96">
       <div className="relative mt-8">
-        <form className="bg-white shadow-md rounded-md px-8 pt-6 pb-8 mt-4 mb-4 ">
+        <form className="bg-white shadow-md rounded-md px-8 pt-6 pb-8 mt-4 mb-4  relative">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 mb-4 flex justify-end"
+            viewBox="0 0 20 20"
+            fill="none"
+            stroke="black"
+            cursor="pointer"
+          >
+            <path
+              fill="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+              clip="evenodd"
+            />
+          </svg>
+
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-semibold mb-2"
@@ -43,7 +58,7 @@ export default function MyForm(props) {
               Message
             </label>
             <textarea
-              className="shadow appearance-none border border   -500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border border-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="message"
               type="message"
               rows={5}
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import logoPic from "../public/logoPic.svg";
 import MyForm from "../components/MyForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,13 +14,15 @@ export default function Home() {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <main className="bg-gradient-to-tr from-slate-900 to-indigo-800 text-white w-screen h-screen flex justify-center items-center flex-col">
-        <Image
-          src={logoPic}
-          alt="logoPic"
-          width="128"
-          height="128"
-          className="cursor-pointer"
-        />
+        <Link href="/">
+          <Image
+            src={logoPic}
+            alt="logoPic"
+            width="128"
+            height="128"
+            className="cursor-pointer"
+          />
+        </Link>
         <h1 className="text-3xl font-normal cursor-pointer ">Contact</h1>
         <MyForm />
       </main>
