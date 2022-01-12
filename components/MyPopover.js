ec8a5dcd49123566a9134d6feb2b3fd446c90ed5
Eloka Agu <elokaagu@Elokas-MacBook-Pro.local>
2022-01-12 15:19:30 +0000

    button and popover

---
+++ components/MyPopover.js
@@ -0,0 +1,105 @@
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { Fragment } from "react";
+
+const getInTouch = [
+  {
+    name: "Contact",
+    description: "Get in touch here",
+    href: "##",
+    icon: IconOne,
+  },
+];
+
+export default function MyPopover() {
+  return (
+    <div className="relative mt-8">
+      <Popover className="relative">
+        {({ open }) => (
+          <>
+            <Popover.Button
+              className={`
+                ${open ? "" : "text-opacity-90"}
+                text-white group bg-blue-700 px-3 py-2 rounded-md inline-flex items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+            >
+              <span>Product Studio</span>
+              <ChevronDownIcon
+                className={`${open ? "" : "text-opacity-70"}
+                  ml-2 h-5 w-5 text-blue-300 group-hover:text-opacity-80 transition ease-in-out duration-150`}
+                aria-hidden="true"
+              />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute z-10 w-96 max-w-sm p-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 lg:max-w-3xl">
+                <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                  <div className="relative bg-white p-7 ">
+                    {getInTouch.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-50"
+                      >
+                        <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white sm:h-12 sm:w-12">
+                          <item.icon aria-hidden="true" />
+                        </div>
+                        <div className="ml-4">
+                          <p className="text-sm font-medium text-gray-900">
+                            {item.name}
+                          </p>
+                          <p className="text-sm text-gray-500">
+                            {item.description}
+                          </p>
+                        </div>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </div>
+  );
+}
+
+function IconOne() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="8" fill="#4442A5" />
+      <path
+        d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
+        stroke="#FFFF"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
+        stroke="#ECECF2"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
+        stroke="#E5E5E5"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import logoPic from "../public/logoPic.svg";
+import MyPopover from "../components/MyPopover";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <div className="bg-gradient-to-tr from-slate-900 to-indigo-800 text-white w-screen h-screen flex justify-center items-center flex-col  ">
+      <main className="bg-gradient-to-tr from-slate-900 to-indigo-800 text-white w-screen h-screen flex justify-center items-center flex-col  ">
         <Image
           src={logoPic}
           alt="logoPic"
@@ -20,7 +21,8 @@ export default function Home() {
           className="cursor-pointer"
         />
         <h1 className="text-3xl font-normal cursor-pointer ">Satellite Beta</h1>
-      </div>
+        <MyPopover />
+      </main>
     </div>
   );
 }
