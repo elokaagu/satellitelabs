819dc39ba88f3bdb006548cef762feb734518a8b
elokaagu <eloka@satellitelabs.xyz>
2022-05-29 17:41:43 +0100

    satellite upgrade complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ export default function Home() {
               <Link href="https://www.keakie.com/discover">
                 <a target="_blank" rel="noopener noreferrer">
                   <Image
-                    src="/keakiesquare.png"
+                    src="/keakiecircle.png"
                     alt="image"
                     height={40}
                     width={40}
@@ -98,10 +98,18 @@ export default function Home() {
         <BottomOverlay>
           <p>
             crafted by{" "}
-            <Link href="/about">
-              <ForwardLink>
-                <a>@eloka</a>
-              </ForwardLink>
+            <Link href="https://www.crunchbase.com/person/eloka-agu">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                  fontWeight: "bold",
+                }}
+              >
+                @eloka
+              </a>
             </Link>{" "}
           </p>
         </BottomOverlay>
@@ -178,6 +186,10 @@ const BottomOverlay = styled.div`
       cursor: pointer;
     }
   }
+
+  > a {
+    text-decoration: none;
+  }
 `;
 
 const VideoContainer = styled.div`
@@ -230,11 +242,12 @@ const ForwardLink = styled.a`
   padding-bottom: 20px;
   text-decoration: none;
   font-weight: bold;
-  :hover {
-    color: gray;
-    cursor: pointer;
-    transition: all 0.5s ease-in-out;
-  }
 `;
 
-const KeakieCircle = styled.div``;
+const KeakieCircle = styled.span`
+  & > span {
+    /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); */
+    border-radius: 999px;
+    object-fit: cover;
+  }
+`;
